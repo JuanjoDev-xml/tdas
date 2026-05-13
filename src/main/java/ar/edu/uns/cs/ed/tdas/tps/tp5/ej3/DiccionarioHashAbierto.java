10e50abf27f7ca9d85cd0071b52dc12a3b9cb7cc
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2026-05-13 19:21:48 -0300

    feat: imports and attributes

--- src/main/java/ar/edu/uns/cs/ed/tdas/tps/tp5/ej3/DiccionarioHashAbierto.java
+++ src/main/java/ar/edu/uns/cs/ed/tdas/tps/tp5/ej3/DiccionarioHashAbierto.java
@@ -1,5 +1,16 @@
 package ar.edu.uns.cs.ed.tdas.tps.tp5.ej3;
 
+import java.security.Key;
+
+import ar.edu.uns.cs.ed.tdas.Entry;
+import ar.edu.uns.cs.ed.tdas.tdalista.PositionList;
+import ar.edu.uns.cs.ed.tdas.Position;
+import ar.edu.uns.cs.ed.tdas.excepciones.InvalidKeyException;
 public class DiccionarioHashAbierto<K,V> implements Dictionary<K,V>{
-    
+    // Atributos de clase
+    protected static final float factorDeCarga = 0.7f;
+    // Atributos de instancia
+    protected PositionList<Entry<K,V>>[] A;
+    protected int N;
+    protected int tamanio;
 }
