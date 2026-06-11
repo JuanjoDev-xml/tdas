be309f2a74d627a4a546889c642c6c98b97ccac2
Juan Jose Denk Guido <juanjosedenk05@gmail.com>
2026-06-11 11:59:31 -0300

    feat: Graph setup

---
+++ src/main/java/ar/edu/uns/cs/ed/tdas/tps/tp7/Vertice.java
@@ -0,0 +1,31 @@
+package ar.edu.uns.cs.ed.tdas.tps.tp7;
+import ar.edu.uns.cs.ed.tdas.tdagrafo.Vertex;
+import ar.edu.uns.cs.ed.tdas.Position;
+import ar.edu.uns.cs.ed.tdas.tdalista.PositionList;
+import ar.edu.uns.cs.ed.tdas.tps.tp4.ej1.ListaDobleEnlazadaCentinela;
+
+public class Vertice<V,E> implements Vertex<V> {
+    private V rotulo;
+    private PositionList<Arco<V,E>> adyacentes;
+    private Position<Vertice<V,E>> posicionEnNodos;
+    public Vertice( V rotulo ) {
+        this.rotulo = rotulo;
+        adyacentes = new ListaDobleEnlazadaCentinela<Arco<V,E>>();
+    }
+    public V element(){
+        return rotulo;
+    }
+    // Setters y getters
+    public void setRotulo(V nuevoRotulo) {
+        rotulo = nuevoRotulo;
+    }
+    public PositionList<Arco<V,E>> getAdyacentes() {
+        return adyacentes;
+    }
+    public void setPosicionEnNodos(Position<Vertice<V,E>> p ) {
+        posicionEnNodos = p;
+    }
+    public Position<Vertice<V,E>> getPosicionEnNodos(){
+        return posicionEnNodos;
+    }
+}
